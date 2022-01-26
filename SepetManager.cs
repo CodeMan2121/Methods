6556cf1c95091a3cadc8721fb448eb2c52e8da41
Administrator <Administrator@KINGHAZE>
2022-01-26 23:16:20 +0300

    Proje dosyası ekle.

---
+++ SepetManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)//public void ile Ekle adında bir metot tanımladık.Bu metot ekrana "Sepete eklendi" yazdırmaktır. 
+        {
+            //convention - isimlendirme
+
+            Console.WriteLine("Sepete eklendi : " + urun.Adi);
+
+        }
+
+        public void Ekle2(string Adi, double Fiyat, string Aciklama)
+        {//Bu şekilde yapmak zaman kaybı oluyor galiba. Çünkü değişken içindeki parametrelere yeni bir tanesi eklenirse her satırda düzeltmek gerek.
+            Console.WriteLine("Sepete eklendi: " + Adi);
+        }
+    }
+}
